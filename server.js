c477b30629752090a9fdb11d5ff58de5d5b55162
Chukwunonso10 <kuzuechinonsojude@gmai.com>
2024-11-28 20:27:37 +0100

    debugging

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(session({
   }),
   cookie: { 
     secure: process.env.NODE_ENV === 'production',
-    httpOnly: true,
+    httpOnly: false,
     maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
     sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
   }
